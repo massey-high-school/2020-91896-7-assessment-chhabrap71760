e86bbcd1202cd8f6b70f686467993199759d8136
chhabrap71760 <53203344+chhabrap71760@users.noreply.github.com>
2020-06-04 09:52:52 +1200

    just began

---
+++ 01_Instructions.py
@@ -0,0 +1,9 @@
+# To Do
+# Ask user if they want to view the instructions otherwise
+# If not, allow them to skip
+
+input("Press <enter> to view the instructions")
+
+print('''Welcome To The Measurement Calculator
+
+''')
