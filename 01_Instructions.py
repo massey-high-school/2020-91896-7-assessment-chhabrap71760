613634ccedc95547cb41e4db2689dc3219348902
chhabrap71760 <53203344+chhabrap71760@users.noreply.github.com>
2020-06-09 21:52:23 +1200

    added instructions (not complete)

--- 01_Instructions.py
+++ 01_Instructions.py
@@ -4,6 +4,16 @@
 
 input("Press <enter> to view the instructions")
 
-print('''Welcome To The Measurement Calculator
+print('''
+Welcome To The Measurement Calculator
+
+This program is a tool that you can use for your measurement homework.
+All you have to do is choose your desired shape like squares, rectangles, 
+triangles, parallelograms and circles. Then the type of measurement 
+you want like area, perimeter, diameter, radius and circumference.
+It will the calculate for your
+
+
+
 
 ''')
